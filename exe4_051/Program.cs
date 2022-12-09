2b63de1d3a165eaa71b49e4ef782da4c3b860a20
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-09 15:34:20 +0700

    Add project files.

---
+++ exe4_051/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe4_051
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
