e93cadd22fe180bb358ec4e5506706415554bcc7
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-09 16:02:10 +0700

    add list stack menu

--- exe4_051/Program.cs
+++ exe4_051/Program.cs
@@ -69,7 +69,19 @@ namespace exe4_051
             Stack s = new Stack();
             while (true)
             {
+                Console.WriteLine();
+                Console.WriteLine("\n *Stack Menu*\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\n Enter your choice :");
+                string input = Console.ReadLine();
+                char ch = Convert.ToChar(input == "" ? "0" : input);
+                switch (ch)
+                {
 
+                }
             }
         }
         
