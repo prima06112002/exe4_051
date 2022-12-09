aaff0ac65cf431ddbc4e700c8d99655037ea3540
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-09 15:39:06 +0700

    add public stack

--- exe4_051/Program.cs
+++ exe4_051/Program.cs
@@ -8,6 +8,15 @@ namespace exe4_051
 {
     class Stack
     {
+        private String[] ele = new string[40];
+        private int top;
+        private int max;
+
+        public Stack()
+        {
+            top = -1;
+            max = ele.Length;
+        }
         
     }
 }
